7d66d6f009471dbc91fc84928b86792f516299c2
Tachera Sasi <tacherasasi@gmail.com>
2025-01-06 16:02:10 +0300

    Blog page

---
+++ app/blog/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "../globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Tachera W Sasi",
+  description: "Tachera W Sasi's portfolio. A passionate web developer and Golang enthusiast with expertise in modern web technologies. CEO @ ekilie.",
+  keywords:"Tachera W Sasi,Tach, ekilie ceo,sasi, web developer, Golang, PHP, Python, TypeScript, Java, C++, JavaScript, Django, React, Node.js, Next.js, React Native, PostgreSQL, Docker, Gin, Fiber, Express.js, Hono, Ubuntu, VSCode, NeoVim, Prisma, HTMX"
+};
+
+interface Props{
+  children:React.ReactNode
+}
+
+export default function BlogLayout({children}:Readonly<Props>){
+  return(
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  )
+}
---
+++ app/blog/page.tsx
@@ -0,0 +1,7 @@
+export default function BlogPage(){
+  return (
+    <div>
+      blog page
+    </div>
+  )
+}
