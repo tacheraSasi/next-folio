475ba9565809ca491bcaa55670d679e61588772a
Tachera Sasi <tacherasasi@gmail.com>
2025-09-08 00:51:08 +0300

    fix: update eslint configuration to disable specific rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,8 +9,17 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
+
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      all: "off",
+      '@next/next/no-img-element': 'off',
+      'prefer-const': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
+  },
 ];
 
 export default eslintConfig;
